39fee07c1d68da51b097d80dc82a8319a0a6b3ea
Hiroki Horiuchi <x19290@gmail.com>
2025-01-30 13:48:27 +0900

    -

--- music/qr.sh
+++ music/qr.sh
@@ -2,7 +2,7 @@
 
 ./qr_with_title.py "ファ譜画像" "https://efvock.github.io/music/fa.png" "fa-score"
 ./qr_with_title.py "ファ譜を演奏" "https://efvock.github.io/music/fa.mp3" "fa-play"
-./qr_with_title.py "ラ譜を演奏" "https://efvock.github.io/music/la.png" "la-score"
+./qr_with_title.py "ラ譜を画像" "https://efvock.github.io/music/la.png" "la-score"
 ./qr_with_title.py "ラ譜を演奏" "https://efvock.github.io/music/fa.mp3" "la-play"
 ./qr_with_title.py "整形済みチャット" "https://efvock.github.io/music/enharmonic-chat" "enharmonic-chat"
 ./qr_with_title.py "生チャット(長い)" "https://chatgpt.com/share/67999ec4-ed98-8012-b243-3bea4f41f83c" "enharmonic-chat-raw"
