4acccb8b1490fa12934cca411e93adc54a6c47f9
Hiroki Horiuchi <x19290@gmail.com>
2025-08-17 17:08:17 +0900

    -

--- google-api/xauth.py
+++ google-api/xauth.py
@@ -1,16 +1,20 @@
-#!/usr/bin/env python3
-
 """
 >>> SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
->>> bool(google_service(SCOPES, "gmail", "v1"))
+>>> creds = xauth(SCOPES)
+>>> bool(google_service("gmail", "v1", creds))
 True
+>>> bool(google_service("gmail", "v1", creds))
+True
+>>> bool(google_service_ex(SCOPES, "gmail", "v1"))
+True
+
 >>> SCOPES = ["https://www.googleapis.com/auth/drive.file"]
->>> bool(google_service(SCOPES, "drive", "v3"))
+>>> bool(google_service_ex(SCOPES, "drive", "v3"))
 True
 """
 
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google_auth_oauthlib.flow import InstalledAppFlow as AppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from pathlib import Path
@@ -28,14 +32,19 @@ def xauth(scopes):
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(credentials_json, scopes)
+            flow = AppFlow.from_client_secrets_file(credentials_json, scopes)
             creds = flow.run_local_server(port=0)
         token_json.write_text(creds.to_json())
     return creds
 
 
-def google_service(scopes, service, version):
-    return build(service, version, credentials=xauth(scopes))
+def google_service(service, version, creds):
+    return build(service, version, credentials=creds)
+
+
+def google_service_ex(scope, service, version):
+    creds = xauth(scope)
+    return google_service(service, version, creds)
 
 
 if __name__ == "__main__":
