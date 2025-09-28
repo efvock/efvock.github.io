c77faa2fdcb9604baf6f863b0cdb8e13b99f365b
Hiroki Horiuchi <x19290@gmail.com>
2025-09-28 15:48:19 +0900

    -

--- amz.py
+++ amz.py
@@ -2,56 +2,60 @@
 
 from pathlib import Path
 from dateutil import parser
-import pytz
 import csv
 
-LARGE_SCHEME = "Order Date", "Total Owed", "Product Name"
-SHORT_SCHEME_KEN = dict(date="利用日", shop="利用店名・商品名", amount="支払総額")
+LARGE_SCHEMA = "Order Date", "Total Owed", "Product Name"
+SMALL_SCHEMA_KEN = dict(date="利用日", amount="支払総額", shop="利用店名・商品名")
 DOWNLOADS = Path("~/Downloads").expanduser()
 KEN = DOWNLOADS / "Your Orders"
 H2 = DOWNLOADS / "Your Orders (1)"
-LARGELOG = "Retail.OrderHistory.3/Retail.OrderHistory.3.csv"
+LARGE_LOG_SUFFIX = "Retail.OrderHistory.3/Retail.OrderHistory.3.csv"
+LARGE_LOG_PREFIX = dict(ken=KEN, h2=H2)
 
 
-def largelog(whose):
-    return dict(ken=KEN, h2=H2)[whose] / LARGELOG
+def large_log(whose):
+    return LARGE_LOG_PREFIX[whose] / LARGE_LOG_SUFFIX
+
 
 def simplify_date(date_string):
     return parser.parse(date_string).strftime("%Y-%m-%d")
 
-def date_range(smalllog, scheme):
-    with smalllog.open() as iobj:
+
+def date_range(small_log, schema):
+    with small_log.open() as iobj:
         rdr = csv.reader(iobj)
         firstline = rdr.__next__()
         rows = []
-        for row in "date", "shop", "amount":
-            name = scheme[row]
-            rows.append(firstline.index(name))
+        for row_en in "date", "amount", "shop":
+            row_label = schema[row_en]
+            rows.append(firstline.index(row_label))
         for row in rdr:
-            if row[1].strip().lower() == "amazon.co.jp":
-                datestring = row[0]
-                yield datestring
+            shop = row[rows[2]].strip().lower()
+            if shop == "amazon.co.jp":
+                date_string = row[rows[0]]
+                yield date_string
 
 
 def large_subset(whose, start, end):
-    large = largelog(whose)
+    large = large_log(whose)
     with large.open() as iobj:
         rdr = csv.reader(iobj)
         firstline = rdr.__next__()
-        date_index = firstline.index(LARGE_SCHEME[0])
-        amount_index = firstline.index(LARGE_SCHEME[1])
-        product_index = firstline.index(LARGE_SCHEME[2])
+        date_index = firstline.index(LARGE_SCHEMA[0])
+        amount_index = firstline.index(LARGE_SCHEMA[1])
+        product_index = firstline.index(LARGE_SCHEMA[2])
         for row in rdr:
             date = row[date_index]
             date = simplify_date(date)
             if start <= date <= end:
                 yield date, row[amount_index], row[product_index]
 
+
 def main():
     from datetime import timedelta
 
     one_day = timedelta(days=1)
-    dates = tuple(date_range(DOWNLOADS / "enavi.csv", SHORT_SCHEME_KEN))
+    dates = list(date_range(DOWNLOADS / "enavi.csv", SMALL_SCHEMA_KEN))
     end, start = dates[0], dates[-1]
     start, end = (parser.parse(y) - one_day for y in (start, end))
     start, end = (y.strftime("%Y-%m-%d") for y in (start, end))
